8d4fc436e568711285a2c652a2ea2c6a5ecb12c4
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2024-05-30 13:43:17 +0200

    Adding django Framework

---
+++ hello_world.py
@@ -0,0 +1,1 @@
+print('Hola. Ya estoy aquí')
---
+++ scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\zCODE\12_DJANGO\00_django_rest_api_project\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
